03e7de870a422d64b0f9611727e03227f666b618
Phamminhtrikp <tripham.210504@gmail.com>
2024-12-31 23:50:28 +0700

    #tri: create order model

---
+++ app/Models/Order.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    public function getId() {
+        return $this->attributes['id'];
+    }
+
+    public function setId($id) {
+        $this->attributes['id'] = $id;
+    }
+
+    public function getUId() {
+        return $this->attributes['uid'];
+    }
+
+    public function setUId($uid) {
+        $this->attributes['uid'] = $uid;
+    }
+
+    public function getShippingAddressId(){
+        return $this->attributes['shippingAddressId'];
+    }
+
+    public function setShippingAddressId($shippingAddressId) {
+        $this->attributes['shippingAddressId'] = $shippingAddressId;
+    }
+
+    public function getStatus(){
+        return $this->attributes['status'];
+    }
+
+    public function setStatus($status) {
+        $this->attributes['status'] = $status;
+    }
+
+    public function getTotalPrice() {
+        return $this->attributes['totalPrice'];
+    }
+
+    public function setTotalPrice($totalPrice) {
+        $this->attributes['totalPrice'] = $totalPrice;
+    }
+
+    public function getCreatedAt(){
+        return $this->attributes['created_at'];
+    }
+
+    public function setCreatedAt($createdAt) {
+        $this->attributes['created_at'] = $createdAt;
+    }
+
+    public function getUpdatedAt(){
+        return $this->attributes['updated_at'];
+    }
+
+    public function setUpdatedAt($updatedAt) {
+        $this->attributes['updated_at'] = $updatedAt;
+    }
+    public function getDeletedAt(){
+        return $this->attributes['deleted_at'];
+    }
+
+    public function setDeletedAt($deletedAt) {
+        $this->attributes['deleted_at'] = $deletedAt;
+    }
+}
