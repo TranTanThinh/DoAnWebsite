33b4ed2aea80550724b8bbdb81a29416a95785b0
huathanhnhat <0306221048@gmail.com>
2025-01-06 04:11:23 +0700

    #Nhat: add file CartController.php

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function index()
+    {
+        $carts = Cart::with('product')->where('user_id', auth()->id())->get();
+        return view('Template.pages.cart', compact('carts'));
+    }
+}
