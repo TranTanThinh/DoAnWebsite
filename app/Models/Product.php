e04ae6ec85e5dca6ef06e3a5d02e6eccd82dd329
huathanhnhat <0306221048@gmail.com>
2025-01-06 00:43:46 +0700

    #Nhat: Edit Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -17,7 +17,7 @@ class Product extends Model
      * $this->attributes['created_at'] - timestamp - contains the product creation date
      * $this->attributes['updated_at'] - timestamp - contains the product update date
     **/
-
+    protected $table = 'products';
     protected $fillable = [
         'name',
         'image',
@@ -26,72 +26,6 @@ class Product extends Model
         'slug',
         'created_at',
         'updated_at',
-        'deleted_at'];
-
+        'deleted_at'
     ];
-
-
-    public function getId() {
-        return $this->attributes['id'];
-    }
-
-    public function setId($id) {
-        $this->attributes['id'] = $id;
-    }
-
-    public function getName() {
-        return $this->attributes['name'];
-    }
-
-    public function setName($name) {
-        $this->attributes['name'] = $name;
-    }
-
-    public function getImage() {
-        return $this->attributes['image'];
-    }
-
-    public function setImage($image) {
-        $this->attributes['image'] = $image;
-    }
-
-    public function getDescription() {
-        return $this->attributes['description'];
-    }
-
-    public function setDescription($description) {
-        $this->attributes['description'] = $description;
-    }
-
-    public function getPrice() {
-        return $this->attributes['price'];
-    }
-
-    public function setPrice($price) {
-        $this->attributes['price'] = $price;
-    }
-
-    public function getSlug() {
-        return $this->attributes['slug'];
-    }
-
-    public function setSlug($slug) {
-        $this->attributes['slug'] = $slug;
-    }
-
-    public function getCreatedAt(){
-        return $this->attributes['created_at'];
-    }
-
-    public function setCreatedAt($createdAt) {
-        $this->attributes['created_at'] = $createdAt;
-    }
-
-    public function getUpdatedAt(){
-        return $this->attributes['updated_at'];
-    }
-
-    public function setUpdatedAt($updatedAt) {
-        $this->attributes['updated_at'] = $updatedAt;
-    }
 }
