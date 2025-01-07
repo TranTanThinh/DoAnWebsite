6daaf8cedb18ba10cc4524ad6c294658aa4f5421
kiet <kiet02032004@gmail.com>
2025-01-07 09:27:16 +0700

    M

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,20 @@
 <?php
 
-use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
-use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\ProductController;
-
-use App\Http\Controllers\Auth\RegisterController;
 
 Route::controller(HomeController::class)->group(function () {
     Route::get('/', 'index')->name('index');
     Route::get('/index', 'index');
     Route::get('/blog', 'blog')->name('blog');
     Route::get('/contact', 'contact')->name('contact');
-    // Route::get('/shop', 'shop')->name('shop');
+    Route::get('/shop', 'shop')->name('shop');
     Route::get('/cart', 'cart')->name('cart');
     Route::get('/blogsingle', 'blogsingle')->name('blogsingle');
-    // Route::get('/productsingle', 'productsingle')->name('productsingle');
+    Route::get('/productsingle', 'productsingle')->name('productsingle');
     Route::get('/checkout', 'checkout')->name('checkout');
     Route::get('/about', 'about')->name('about');
     Route::get('/wishlist', 'wishlist')->name('wishlist');
 });
-
-
-Auth::routes();
-use App\Http\Controllers\Auth\LoginController;
-
-Route::post('/login', [LoginController::class, 'login'])->name('login');
-Route::get('register', [RegisterController::class, 'showRegisterForm'])->name('register');
-Route::post('register', [RegisterController::class, 'register']);
-//Route::get('/profile', [UserController::class, 'profile'])->name('profile');
-Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
-
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::controller(ProductController::class)->group(function () {
-    route::get('/shop', 'index')->name('product.index');
-    route::get('products/{slug}/{id}', 'show')->name('product.show');
-});
-
-Route::controller(CartController::class)->group(function() {
-    route::get('/cart', 'index')->name('cart.index');
-    route::get('/cart/delete','delete')->name('cart.delete');
-    route::post('/cart/add/{id}','add')->name('cart.add');
-});
-
+Route::resource('/admin', AdminController::class);
