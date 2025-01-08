9aa5abf955d3ffd35c37eff4b21ac49356329f68
diachudaden <0306221051@caothang.edu.vn>
2025-01-08 21:33:08 +0700

    thêm phương thức thêm danh mục sản phẩm

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use App\Models\Category;
 class CategoryController extends Controller
 {
-
     public function index()
     {
         $category = Category::paginate(10);
