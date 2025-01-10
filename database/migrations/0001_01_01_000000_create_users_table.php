0c65b17789fca6fa849410414d63c4a4eabd5424
Phamminhtrikp <tripham.210504@gmail.com>
2025-01-10 21:16:14 +0700

    update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,11 +17,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-<<<<<<< HEAD
-            $table->string('role');
-=======
             $table->string('role')->default('user');
->>>>>>> thinh
             $table->string('phone');
             $table->string('firstName');
             $table->string('lastName')->nullable();
