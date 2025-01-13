29657eeedf2df3009ebe0fefe68b6e2f9586921c
TranTanThinh <trantanthinh1324@gmail.com>
2025-01-14 00:58:50 +0700

    Update Payments

--- resources/views/Template/partials/header.blade.php
+++ resources/views/Template/partials/header.blade.php
@@ -83,8 +83,8 @@
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item"><a href="{{ route('index') }}" class="nav-link">Home</a></li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="{{ route('product.index') }}" id="dropdown04" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">Shop</a>
+                        <a class="nav-link dropdown-toggle" href="{{ route('product.index') }}" id="dropdown04"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Shop</a>
                         <div class="dropdown-menu" aria-labelledby="dropdown04">
                             <a class="dropdown-item" href="blog">Blog</a>
                             <a class="dropdown-item" href="wishlist">Wishlist</a>
@@ -235,11 +235,10 @@
                             </div>
 
                         </div>
-                        <div id="error-message" style="color: red; display: none;">Confirm Password do not match.
-                        </div>
-                        <button type="submit" class="btn btn-primary">Register</button>
-                    </form>
                 </div>
+                <button type="submit" class="btn btn-primary">Register</button>
+                </form>
             </div>
         </div>
+    </div>
     </div>
